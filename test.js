e0f7d07adc3afdf6b8da0a65da98386b25a3d928
Filipe Linhares <lip.linhares@gmail.com>
2015-10-09 10:44:53 -0300

    Removes test files

--- test.js
+++
@@ -1,7 +0,0 @@
-var Doki = require('./index.js');
-var dss  = require('dss');
-
-doki = new Doki('./demo/scss/*.scss');
-doki.parser('id', (i, line, block) => line );
-
-doki.parse('./docs/index.json');
